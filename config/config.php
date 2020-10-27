87ba8e0d354f1e0aaa2cc69d4a10af5a81aab91c
Fabien Zoccola <biothewolff@gmx.fr>
2020-10-28 00:11:24 +0100

    feat(config): implementing paths config

--- config/config.php
+++ config/config.php
@@ -2,4 +2,20 @@
 
 return [
 
+    # User session
+    'path.login' => '/login',
+    'path.logout' => '/logout',
+
+    # CAS: service ticket validation
+    'path.validate' => '/validate',  # CAS 1.0
+    'path.service-validate' => '/serviceValidate',  # CAS 2.0
+    'path.service-validate.p3' => '/p3/serviceValidate',  # CAS 3.0
+
+    # CAS: proxy ticket service (CAS 2.0)
+    'path.proxy' => '/proxy',
+
+    # CAS: service/proxy ticket validation
+    'path.proxy-validate' => '/proxyValidate',  # CAS 2.0
+    'path.proxy-validate.p3' => '/p3/proxyValidate'  # CAS 3.0
+
 ];
